dcc6872107042faa5c1ec6db13f72b122dd1d366
Konamo-Sorano <ogaro3059@gmail.com>
2026-01-12 09:09:59 +0300

    some Freaky monkeys and areas before monkey white done still WIP logic

--- scripts/init.lua
+++ scripts/init.lua
@@ -35,6 +35,7 @@ if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
     Tracker:AddLocations("locations/level maps/seaside resort.jsonc")
     Tracker:AddLocations("locations/level maps/Hide-n-Seek.jsonc")
     Tracker:AddLocations("locations/level maps/Saru-mon's Castle.jsonc")
+    Tracker:AddLocations("locations/level maps/Freaky Monkey Five.jsonc")
 end
 
 -- Layout
